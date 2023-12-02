ca92e445a59b3779a49c29aa79902d4b5d836beb
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2023-12-02 18:28:20 +0500

    Update Password Function.c

--- Password Function.c
+++ Password Function.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void password ()
 {
-	int password = 54321, cnt = 0, i, userpassword;
+	int password = 54321, password2 = 12345, password3 = 80808, cnt = 0, i, userpassword;
 	printf("Welcome to BaseOps Pro!\nEnter the password: ");
 	for (i=1; i<=3; i++)
 	{
 		scanf("%d", &userpassword);
-		if(userpassword != password)
+		if(userpassword != password && userpassword != password1 && userpassword != password3)
 		{
 			if(i == 1 || i == 2){
 				printf("\nYou have entered wrong password!\nTry Again.... (%d tries left)\n", 3-i);
