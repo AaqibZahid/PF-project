351dc7516f96707b59679a68dad4b5748870302a
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2023-12-02 19:11:36 +0500

    Update Password Function.c

--- Password Function.c
+++ Password Function.c
@@ -6,7 +6,7 @@ void password ()
 	for (i=1; i<=3; i++)
 	{
 		scanf("%d", &userpassword);
-		if(userpassword != password && userpassword != password1 && userpassword != password3)
+		if(userpassword != password && userpassword != password2 && userpassword != password3)
 		{
 			if(i == 1 || i == 2){
 				printf("\nYou have entered wrong password!\nTry Again.... (%d tries left)\n", 3-i);
